07d7bf6ed41cc10b5a0db85e02b53065730dda83
diamant1991 <diamant1988@mail.ru>
2015-05-22 15:13:46 +0300

    add other files

--- js/script.js
+++ js/script.js
@@ -25,3 +25,72 @@ $('.slct').click(function(){
 	}
 	return false;
 });
+
+$(document).ready(function () {
+	$('input,textarea').focus(function(){
+	  $(this).data('placeholder',$(this).attr('placeholder'))
+	  $(this).attr('placeholder','');
+	});
+	$('input,textarea').blur(function(){
+	  $(this).attr('placeholder',$(this).data('placeholder'));
+	});
+
+
+	$('.city-link').click(function(){
+  	 $('.form-mask').fadeIn(400);
+  	 $('#cities').fadeIn(400);
+  })
+	$('.get-call-link').click(function(){
+  	 $('.form-mask').fadeIn(400);
+  	 $('#get-call').fadeIn(400);
+  })
+	$('.ques-link').click(function(){
+  	 $('.form-mask').fadeIn(400);
+  	 $('#get-ques').fadeIn(400);
+  })
+	$('.privacy-link').click(function(){
+  	 $('.form-mask').fadeIn(400);
+  	 $('#privacy').fadeIn(400);
+  })
+	$('.pvz-link').click(function(){
+  	 $('.form-mask').fadeIn(400);
+  	 $('#pvz').fadeIn(400);
+  })
+	$('.payment-link').click(function(){
+  	 $('.form-mask').fadeIn(400);
+  	 $('#payment').fadeIn(400);
+  })
+	$('.delivery-link').click(function(){
+  	 $('.form-mask').fadeIn(400);
+  	 $('#delivery').fadeIn(400);
+  })
+	$('.post-link').click(function(){
+  	 $('.form-mask').fadeIn(400);
+  	 $('#post').fadeIn(400);
+  })
+	$('.express-link').click(function(){
+  	 $('.form-mask').fadeIn(400);
+  	 $('#express').fadeIn(400);
+  })
+
+
+	$('.close-btn,.form-mask,.close-link').click(function(){
+  	 $('.modal,.form-mask').fadeOut(400);
+  })
+	$(document).keyup(function(d) {
+	    if (d.keyCode == 27) {
+	        $('.form-mask').fadeOut(400);
+	        $('.modal').fadeOut(400);
+	    }
+	});
+
+	$("a.toplink").click(function() {
+      $("html, body").animate({
+         scrollTop: $($(this).attr("href")).offset().top - 38
+      }, {
+         duration: 1000,
+         easing: "swing"
+      });
+      return false;
+   });
+});
